30f4abd87ff0a98379b7b0221119bd52ffcb8004
data-tadumadze <dtadu2018@agruni.edu.ge>
2023-02-27 22:21:58 +0400

    add angular homework4

---
+++ index.js
@@ -0,0 +1,120 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var WeekDays;
+(function (WeekDays) {
+    WeekDays["Monday"] = "Monday";
+    WeekDays["Tuesday"] = "Tuesday";
+    WeekDays["Wednesday"] = "Wednesday";
+    WeekDays["Thursday"] = "Thursday";
+    WeekDays["Friday"] = "Friday";
+    WeekDays["Saturday"] = "Saturday";
+    WeekDays["Sunday"] = "Sunday";
+})(WeekDays || (WeekDays = {}));
+var WeatherTerms;
+(function (WeatherTerms) {
+    WeatherTerms["Sunny"] = "Sunny";
+    WeatherTerms["Cloudy"] = "Cloudy";
+    WeatherTerms["PartlyCloudy"] = "Partly Cloudy";
+    WeatherTerms["Rainy"] = "Rainy";
+    WeatherTerms["Snowy"] = "Snowy";
+    WeatherTerms["Foggy"] = "Foggy";
+    WeatherTerms["Windy"] = "Windy";
+    WeatherTerms["Thunderstorm"] = "Thunderstorm";
+})(WeatherTerms || (WeatherTerms = {}));
+var VirtualStorage = /** @class */ (function () {
+    function VirtualStorage() {
+        this.items = [];
+    }
+    VirtualStorage.prototype.add = function (item) {
+        this.items.push(item);
+    };
+    VirtualStorage.prototype.remove = function (id) {
+        var index = this.items.findIndex(function (item) { return item.id === id; });
+        if (index !== -1) {
+            this.items.splice(index, 1);
+        }
+    };
+    VirtualStorage.prototype.update = function (id, updates) {
+        //Partial - ??
+        var index = this.items.findIndex(function (item) { return item.id === id; });
+        if (index !== -1) {
+            this.items[index] = __assign(__assign({}, this.items[index]), updates);
+        }
+    };
+    // find(query: Partial<T>) {
+    //   return this.items.filter((item) => {
+    //     for (const key in query) {
+    //       if (item[key] !== query[key]) {
+    //         return false;
+    //       }
+    //       return true;
+    //     }
+    //   });
+    // }
+    // findOne(query: Partial<T>) {
+    //   return this.items.find((item) => {
+    //     for (const key in query) {
+    //       if (item[key] !== query[key]) {
+    //         return false;
+    //       }
+    //     }
+    //     return true;
+    //   });
+    // }
+    VirtualStorage.prototype.count = function () {
+        return this.items.length;
+    };
+    VirtualStorage.prototype.sumOfBodyLengths = function () {
+        return this.items.reduce(function (sum, item) { return sum + item.temperature; }, 0);
+    };
+    return VirtualStorage;
+}());
+//Add
+var weatherStorage = new VirtualStorage();
+weatherStorage.add({
+    id: 1,
+    country: "Georgia",
+    city: "Tbilisi",
+    gmt: "UTC +4H",
+    weathertype: "Cloudy",
+    temperature: 11,
+    humidity: 52,
+    pressure: 1020,
+    wind: 29,
+    precipitation: 7
+});
+weatherStorage.add({
+    id: 2,
+    country: "Georgia",
+    city: "Batumi",
+    gmt: "UTC +4H",
+    weathertype: "Rainy",
+    temperature: 16,
+    humidity: 87,
+    pressure: 1022,
+    wind: 34,
+    precipitation: 95
+});
+//Remove
+weatherStorage.remove(2);
+//Update
+weatherStorage.update(1, {
+    temperature: 14,
+    humidity: 91,
+    precipitation: 97
+});
+//Find all weather info in Tbilisi
+//   const currentCity = weatherStorage.find({ city: "Tbilisi" });
+//Find a single weather info in Tbilisi
+//   const firstCity = weatherStorage.findOne({ city: "Tbilisi" });
+//Count the total number of cities in storage
+var totalCities = weatherStorage.count();
---
+++ index.ts
@@ -0,0 +1,136 @@
+enum WeekDays {
+    Monday = "Monday",
+    Tuesday = "Tuesday",
+    Wednesday = "Wednesday",
+    Thursday = "Thursday",
+    Friday = "Friday",
+    Saturday = "Saturday",
+    Sunday = "Sunday",
+  }
+
+  enum WeatherTerms {
+    Sunny = "Sunny",
+    Cloudy = "Cloudy",
+    PartlyCloudy = "Partly Cloudy",
+    Rainy = "Rainy",
+    Snowy = "Snowy",
+    Foggy = "Foggy",
+    Windy = "Windy",
+    Thunderstorm = "Thunderstorm",
+  }
+  
+  interface CurrentWeather {
+    id: number;
+    country: string;
+    city: string;
+    gmt: string;
+    weathertype: string;
+    temperature: number;
+    humidity: number;
+    pressure: number;
+    wind: number;
+    precipitation: number;
+  }
+
+class VirtualStorage<T extends CurrentWeather>{
+    //private - ??
+items: CurrentWeather[];
+constructor() {
+    this.items = [];
+}
+add(item: T) {
+    this.items.push(item)
+}
+    remove(id: number) {
+      const index = this.items.findIndex((item) => item.id === id);
+      if (index !== -1) {
+        this.items.splice(index, 1);
+      }
+    }
+    update(id: number, updates: Partial<T>) {
+        //Partial - ??
+      const index = this.items.findIndex((item) => item.id === id);
+      if (index !== -1) {
+        this.items[index] = { ...this.items[index], ...updates };
+      }
+    }
+    find(query: Partial<T>) {
+      return this.items.filter((item) => {
+        for (const key in query) {
+          if (item[key] !== query[key]) {
+            return false;
+          }
+          return true;
+        }
+      });
+    }
+
+    findOne(query: Partial<T>) {
+      return this.items.find((item) => {
+        for (const key in query) {
+          if (item[key] !== query[key]) {
+            return false;
+          }
+        }
+        return true;
+      });
+    }
+    count() {
+      return this.items.length;
+    }
+    sumOfBodyLengths() {
+      return this.items.reduce((sum, item) => sum + item.temperature, 0);
+    }
+  }
+
+    //Add
+  const weatherStorage = new VirtualStorage();
+  weatherStorage.add(
+        {
+            id: 1,
+            country: "Georgia",
+            city: "Tbilisi",
+            gmt: "UTC +4H",
+            weathertype: "Cloudy",
+            temperature: 11,
+            humidity: 52,
+            pressure: 1020,
+            wind: 29,
+            precipitation: 7,
+        }
+   
+  );
+  
+  weatherStorage.add(
+    {
+    id: 2,
+    country: "Georgia",
+    city: "Batumi",
+    gmt: "UTC +4H",
+    weathertype: "Rainy",
+    temperature: 16,
+    humidity: 87,
+    pressure: 1022,
+    wind: 34,
+    precipitation: 95,
+    }
+  )
+  
+  //Remove
+  weatherStorage.remove(2);
+  
+  //Update
+  weatherStorage.update(1, {
+    temperature: 14,
+    humidity: 91,
+    precipitation: 97,
+  });
+  
+  //Find all weather info in Tbilisi
+//   const currentCity = weatherStorage.find({ city: "Tbilisi" });
+  
+  //Find a single weather info in Tbilisi
+//   const firstCity = weatherStorage.findOne({ city: "Tbilisi" });
+  
+  //Count the total number of cities in storage
+  const totalCities = weatherStorage.count();
